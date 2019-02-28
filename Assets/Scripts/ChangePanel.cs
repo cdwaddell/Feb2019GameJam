5b9f07c8fa962b24690fbbb07afe19e887ea6a02
Daniel Waddell <Dwaddell@hmbnet.com>
2019-02-28 09:26:24 -0500

    animations

---
+++ Assets/Scripts/ChangePanel.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ChangePanel : MonoBehaviour
+{
+    public GameObject current;
+    public GameObject desired;
+
+    Animator anim;
+    int normalHash = Animator.StringToHash("Normal");
+    
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TogglePanels()
+    {
+        anim.CrossFade(normalHash, 0f);
+        anim.Update(0f);
+        current.SetActive(false);
+        desired.SetActive(true);
+    }
+}
