80e102118b0d996a9f54323c64efd73f8a2cb319
Daniel Waddell <cdwaddell@gmail.com>
2019-03-03 19:49:33 -0500

    style and flow

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Experimental.UIElements;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Appliances;
+    public GameObject Laundry;
+    public GameObject Customers;
+    public Canvas Canvas;
+}
